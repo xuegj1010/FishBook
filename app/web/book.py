e50741a3acf818516f9ba30d7a46a369e387a108
xuegj1010 <869624090@qq.com>
2019-03-13 23:09:25 +0800

    参数验证

--- app/web/book.py
+++ app/web/book.py
@@ -1,21 +1,23 @@
-from flask import jsonify
+from flask import jsonify, request
 
+from ..forms.book import SearchForm
 from . import web
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
 
 
-@web.route('/book/search/<q>/<page>')
-def search(q, page):
-    """
+@web.route('/book/search')
+def search():
+    form = SearchForm(request.args)
+    if form.validate():
+        q = form.q.data.strip()
+        page = form.page.data
+        isbn_or_key = is_isbn_or_key(q)
+        if isbn_or_key == 'isbn':
+            result = YuShuBook.search_by_isbn(q)
+        else:
+            result = YuShuBook.search_by_keyword(q)
+        return jsonify(result)
+    else :
+        return jsonify({'msg': '参数校验失败'})
 
-    :param q: 普通关键字  isbn
-    :param page:
-    :return:
-    """
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)
-    else:
-        result = YuShuBook.search_by_keyword(q)
-    return jsonify(result)
