b046e7d2bbdecafc73c97ce4dd7e2471b2763847
xuegj1010 <869624090@qq.com>
2019-03-16 21:16:33 +0800

    重构对象

--- app/web/book.py
+++ app/web/book.py
@@ -1,6 +1,8 @@
+import json
+
 from flask import jsonify, request
 
-from app.view_models.book import BookViewModel
+from app.view_models.book import BookCollection
 from ..forms.book import SearchForm
 from . import web
 from app.libs.helper import is_isbn_or_key
@@ -10,17 +12,20 @@ from app.spider.yushu_book import YuShuBook
 @web.route('/book/search')
 def search():
     form = SearchForm(request.args)
+    books = BookCollection()
+
     if form.validate():
         q = form.q.data.strip()
         page = form.page.data
         isbn_or_key = is_isbn_or_key(q)
-        if isbn_or_key == 'isbn':
-            result = YuShuBook.search_by_isbn(q)
-            result = BookViewModel.package_single(result, q)
-        else:
-            result = YuShuBook.search_by_keyword(q, page)
-            result = BookViewModel.package_collection(result, q)
+        yushu_book = YuShuBook()
 
-        return jsonify(result)
+        if isbn_or_key == 'isbn':
+            yushu_book.search_by_isbn(q)
+        else:
+            yushu_book.search_by_keyword(q, page)
+
+        books.fill(yushu_book, q)
+        return json.dumps(books, default=lambda o: o.__dict__)  # 迭代序列化对象
     else:
         return jsonify(form.errors)
